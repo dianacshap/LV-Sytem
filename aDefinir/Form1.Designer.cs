1e74ba59bb314e6cad3b16fd7aff8675afca4098
DIANA CLAUDIA DA SILVA <diana.silva@acesso.edu>
2021-09-14 19:23:58 -0300

    Adicionar arquivos de projeto.

---
+++ aDefinir/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace aDefinir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelTopo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PanelTopo
+            // 
+            this.PanelTopo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.PanelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelTopo.Location = new System.Drawing.Point(0, 0);
+            this.PanelTopo.Name = "PanelTopo";
+            this.PanelTopo.Size = new System.Drawing.Size(915, 43);
+            this.PanelTopo.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 399);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 481);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PanelTopo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel PanelTopo;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
