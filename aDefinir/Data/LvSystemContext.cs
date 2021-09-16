7727479f75075ff60064e4bb11ed07a31d67f6ab
Josué da Motta <josuka360@hotmail.com>
2021-09-16 00:15:57 -0300

    Conexão do banco de dados SQL

---
+++ aDefinir/Data/LvSystemContext.cs
@@ -0,0 +1,24 @@
+﻿using aDefinir.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aDefinir.Data
+{
+    class LvSystemContext : DbContext
+    {
+        public DbSet<Agendamento> Agendamentos { get; set; }
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Servico> Servicos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=LvSystemm;Data Source=WIN10");
+        }
+
+    }
+}
